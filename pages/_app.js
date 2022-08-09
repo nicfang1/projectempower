412057d161427afda250627d123dfcfe9363f38e
nicholas <JVListic@gmail.com>
2022-08-09 14:47:59 +0800

    add google analytics tagging

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import Layout from "../components/layouts/main";
 import Fonts from "../components/fonts";
 import { AnimatePresence } from "framer-motion";
 import Chakra from "../components/chakra";
+import Script from "next/script";
 
 if (typeof window !== "undefined") {
 	window.history.scrollRestoration = "manual";
@@ -10,6 +11,24 @@ if (typeof window !== "undefined") {
 function Website({ Component, pageProps, router }) {
 	return (
 		<Chakra cookies={pageProps.cookies}>
+			<Script
+				strategy="afterInteractive"
+				src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+			/>
+			<Script
+				id="gtag-init"
+				strategy="afterInteractive"
+				dangerouslySetInnerHTML={{
+					__html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+				}}
+			/>
 			<Fonts />
 			<Layout router={router}>
 				<AnimatePresence
